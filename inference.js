a0dde6eb526b3295a19b36ddbb21e7544630cc3f
Tianyi <42670338+tysng@users.noreply.github.com>
2020-03-12 16:22:53 +0800

    Unifed typed_term and type data structure

--- inference.js
+++ inference.js
@@ -1,7 +1,8 @@
-/** Creates a new typed_term tagged list. */
-// TODO: clarify value
-function make_typed_term(type_var, type, value) {
-    return list("typed_term", type_var, type, value);
+/** Creates a new typed_term tagged list. 
+ * Like, Tau1 = BOOL
+*/
+function make_typed_term(type_var, type) {
+    return list("typed_term", type_var, type);
 }
 
 function is_typed_term(stmt) {
@@ -12,9 +13,6 @@ function type_var_of_typed_term(term) {
 }
 function type_of_typed_term(term) {
     return list_ref(term, 2);
-}
-function value_of_typed_term(term) {
-    return list_ref(term, 3);
 }
 
 function init_fresh_type_var() {
@@ -33,23 +31,35 @@ const fresh_type_var = init_fresh_type_var();
 function is_type(stmt) {
     return is_tagged_list(stmt, "type");
 }
-const bool_type = list("type", "bool");
-const number_type = list("type", "number");
+
 const undefined_type = list("type", "undefined");
-/** A var type is the type of a bounded name */
-const var_type = list("type", "var");
-const let_type = list("type", "let");
 
-function new_function_type(param_types, return_type) {
+function new_bool_type(bool) {
+    return list("type", "bool", bool);
+}
+
+function new_number_type(number)  {
+    return list("type", "number", number);
+}
+
+function new_if_type(pred_typed_term, cons_typed_term, alt_typed_term) {
+    return list("type", "if", pred_typed_term, cons_typed_term, alt_typed_term);
+}
+
+function new_function_type(param_typed_term, return_typed_term) {
     // TODO: support multiple params
-    return list("type", "function", param_types, return_type);
+    return list("type", "function", param_typed_term, return_typed_term);
 }
 
-function new_let
-
-function equal_type(type1, type2) {
-    return;
+function new_let_type(name, value_typed_term) {
+    return list("type", "let", name, value_typed_term);
 }
+
+/** A var type is the type of a bounded name */
+function new_var_type(name) {
+    return list("type", "var", name);
+}
+
 
 function annotate(stmt, env) {
     return is_number(stmt)
@@ -60,9 +70,10 @@ function annotate(stmt, env) {
     ? make_typed_term(fresh_type_var(), undefined_type, stmt)
     : is_name(stmt)
     ? annotate_name(stmt, env)
-    // WIP
     : is_constant_declaration(stmt, env)
-    ? 
+    ? annotate_constant_declaration(stmt, env)
+    : is_conditional_expression(stmt, env)
+    ? annotate_conditional_expression(stmt, env)
 
 }
 
@@ -71,6 +82,17 @@ function annotate_name(stmt, env) {
 
 }
 
+function annotate_conditional_expression(stmt, env) {
+    const pred_type = type(cond_expr_pred(stmt), env);
+    const cons_type = type(cond_expr_cons(stmt), env);
+    const alt_type = type(cond_expr_alt(stmt), env);
+
+
+}
+
+/**
+ * Returns a TypedTerm
+ */
 function annotate_constant_declaration(stmt, env) {
     const name = constant_declaration_name(stmt);
     const val = constant_declaration_value(stmt);
@@ -80,7 +102,7 @@ function annotate_constant_declaration(stmt, env) {
         const body = function_definition_body(val);
 
         const typed_params = map(
-            x=>make_typed_term(fresh_type_var(), var_type,x),
+            x=>make_typed_term(fresh_type_var(), new_var_type(x)),
             parameters);
 
         // extend env to evaluate function body
@@ -96,15 +118,15 @@ function annotate_constant_declaration(stmt, env) {
                 annotate(body, extended_env)));
 
         set_type(name, function_def_type, env);
-        return make_typed_term(fresh_type_var, let_type, function_def_type);
+        return make_typed_term(fresh_type_var(), new_let_type(name, function_def_type));
     } else {
         // bind that variable in the env
-        const value_type = annotate(val,env);
+        const value_typed_term = annotate(val,env);
         set_type(
-            name_of_name(type_declaration_name(stmt)),
-            value_type,
+            name,
+            value_typed_term,
             env
         )
-        return make_typed_term(fresh_type_var(), let_type, value_type);
+        return make_typed_term(fresh_type_var(), new_let_type(name, value_typed_term));
     }
 }
