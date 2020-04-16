ffcd59dfcc0e89c9df46b86f9dc0929706f2bdb0
Tianyi <42670338+tysng@users.noreply.github.com>
2020-04-16 13:51:58 +0800

    Format code

--- solve.js
+++ solve.js
@@ -1,20 +1,20 @@
 function sigma(t, sfs) {
-	if (is_base_type(t)) {
-		return t;
-	} else if (is_function_type(t)) {
-		const params = param_types_of_fn_type(t);
-		const res = return_type_of_fn_type(t);
-		const sig_params = map((param) => sigma(param, sfs), params);
-		const sig_res = sigma(res, sfs);
-		return make_function_type(sig_params, sig_res);
-	} else {
-		const find_res = set_find_key_type(sfs, t);
-		if (is_null(find_res)) {
-			return t;
-		} else {
-			return sigma(tail(find_res), sfs);
-		}
-	}
+    if (is_base_type(t)) {
+        return t;
+    } else if (is_function_type(t)) {
+        const params = param_types_of_fn_type(t);
+        const res = return_type_of_fn_type(t);
+        const sig_params = map((param) => sigma(param, sfs), params);
+        const sig_res = sigma(res, sfs);
+        return make_function_type(sig_params, sig_res);
+    } else {
+        const find_res = set_find_key_type(sfs, t);
+        if (is_null(find_res)) {
+            return t;
+        } else {
+            return sigma(tail(find_res), sfs);
+        }
+    }
 }
 
 /**
@@ -25,187 +25,187 @@ function sigma(t, sfs) {
  * @param {*} solved_form_set
  */
 function solve(cons, solved_form_set) {
-	// display(cons);
-	const rules_list = list(
-		rule_1,
-		rule_2,
-		rule_3,
-		rule_4,
-		rule_5,
-		rule_6,
-		rule_7,
-		rule_8
-	);
+    // display(cons);
+    const rules_list = list(
+        rule_1,
+        rule_2,
+        rule_3,
+        rule_4,
+        rule_5,
+        rule_6,
+        rule_7,
+        rule_8
+    );
 
-	function solve_rules(r_list) {
-		if (is_null(r_list)) {
-			error("type error: no rules matched");
-		} else {
-		}
+    function solve_rules(r_list) {
+        if (is_null(r_list)) {
+            error("type error: no rules matched");
+        } else {
+        }
 
-		// rule_*(cons, sfs) -> (bool, sfs)
-		const result = head(r_list)(cons, solved_form_set);
-		// (true, sfs) : if matched, and return the sfs
-		// (false, _) : not matched, go to the next rule
-		return head(result) ? tail(result) : solve_rules(tail(r_list));
-	}
+        // rule_*(cons, sfs) -> (bool, sfs)
+        const result = head(r_list)(cons, solved_form_set);
+        // (true, sfs) : if matched, and return the sfs
+        // (false, _) : not matched, go to the next rule
+        return head(result) ? tail(result) : solve_rules(tail(r_list));
+    }
 
-	return solve_rules(rules_list);
+    return solve_rules(rules_list);
 }
 
 // all function has the signature: rule_*(cons, sfs) -> (bool, sfs)
 function rule_1(cons, sfs) {
-	return equal_type(head(cons), tail(cons)) &&
-		head(head(cons)) === "primitive"
-		? pair(true, sfs) // do nothing
-		: pair(false, null);
+    return equal_type(head(cons), tail(cons)) &&
+        head(head(cons)) === "primitive"
+        ? pair(true, sfs) // do nothing
+        : pair(false, null);
 }
 
 function rule_2(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	return head(t) !== "type_variable" && head(ta) === "type_variable"
-		? pair(true, solve(pair(ta, t), sfs))
-		: pair(false, null);
+    const t = head(cons);
+    const ta = tail(cons);
+    return head(t) !== "type_variable" && head(ta) === "type_variable"
+        ? pair(true, solve(pair(ta, t), sfs))
+        : pair(false, null);
 }
 
 function rule_3(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	const sig_ta = sigma(ta, sfs);
+    const t = head(cons);
+    const ta = tail(cons);
+    const sig_ta = sigma(ta, sfs);
 
-	return is_type_var(t) && equal_type(sig_ta, t)
-		? pair(true, sfs)
-		: pair(false, null);
+    return is_type_var(t) && equal_type(sig_ta, t)
+        ? pair(true, sfs)
+        : pair(false, null);
 }
 
 function rule_4(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	const sig_ta = sigma(ta, sfs);
+    const t = head(cons);
+    const ta = tail(cons);
+    const sig_ta = sigma(ta, sfs);
 
-	if (is_type_var(t) && is_function_type(sig_ta)) {
-		// continue
-	} else {
-		return pair(false, null);
-	}
+    if (is_type_var(t) && is_function_type(sig_ta)) {
+        // continue
+    } else {
+        return pair(false, null);
+    }
 
-	// check if t is contained in Σ(t′)
-	if (
-		equal_type(return_type_of_fn_type(sig_ta), t) ||
-		!is_null(
-			filter(
-				(param) => equal_type(param, t),
-				param_types_of_fn_type(sig_ta)
-			)
-		)
-	) {
-		error("type error: rule 4 broken");
-	} else {
-		return pair(false, null);
-	}
+    // check if t is contained in Σ(t′)
+    if (
+        equal_type(return_type_of_fn_type(sig_ta), t) ||
+        !is_null(
+            filter(
+                (param) => equal_type(param, t),
+                param_types_of_fn_type(sig_ta)
+            )
+        )
+    ) {
+        error("type error: rule 4 broken");
+    } else {
+        return pair(false, null);
+    }
 }
 
 function rule_5(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	const sig_ta = sigma(ta, sfs);
+    const t = head(cons);
+    const ta = tail(cons);
+    const sig_ta = sigma(ta, sfs);
 
-	const is_t_Ai = is_type_var(t) && head(tail(t)) === "A";
+    const is_t_Ai = is_type_var(t) && head(tail(t)) === "A";
 
-	const is_sig_ta_addable =
-		equal_type(sig_ta, number_type) || equal_type(sig_ta, string_type);
+    const is_sig_ta_addable =
+        equal_type(sig_ta, number_type) || equal_type(sig_ta, string_type);
 
-	if (is_t_Ai && !is_type_var(sig_ta) && !is_sig_ta_addable) {
-		error("type error: rule 5 broken");
-	} else {
-		return pair(false, null);
-	}
+    if (is_t_Ai && !is_type_var(sig_ta) && !is_sig_ta_addable) {
+        error("type error: rule 5 broken");
+    } else {
+        return pair(false, null);
+    }
 }
 
 function rule_6(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
+    const t = head(cons);
+    const ta = tail(cons);
 
-	const t_eq_taa = set_find_key(sfs, t);
-	if (is_type_var(t) && !is_null(t_eq_taa)) {
-		return pair(true, solve(pair(ta, tail(t_eq_taa)), sfs));
-	} else {
-		return pair(false, null);
-	}
+    const t_eq_taa = set_find_key(sfs, t);
+    if (is_type_var(t) && !is_null(t_eq_taa)) {
+        return pair(true, solve(pair(ta, tail(t_eq_taa)), sfs));
+    } else {
+        return pair(false, null);
+    }
 }
 
 function rule_7(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	let sig_ta = sigma(ta, sfs);
+    const t = head(cons);
+    const ta = tail(cons);
+    let sig_ta = sigma(ta, sfs);
 
-	if (is_type_var(t) && is_null(set_find_key(sfs, t))) {
-		// addable conversion
-		const sig_t = sigma(t, sfs);
-		if (
-			is_type_var(sig_t) &&
-			head(tail(sig_t)) === "A" &&
-			is_type_var(sig_ta) &&
-			head(tail(sig_ta)) === "T"
-		) {
-			sig_ta = change_type_var_to_addable(sig_ta);
-		} else {
-		}
+    if (is_type_var(t) && is_null(set_find_key(sfs, t))) {
+        // addable conversion
+        const sig_t = sigma(t, sfs);
+        if (
+            is_type_var(sig_t) &&
+            head(tail(sig_t)) === "A" &&
+            is_type_var(sig_ta) &&
+            head(tail(sig_ta)) === "T"
+        ) {
+            sig_ta = change_type_var_to_addable(sig_ta);
+        } else {
+        }
 
-		return pair(true, set_insert(sfs, pair(t, sig_ta)));
-	} else {
-		return pair(false, null);
-	}
+        return pair(true, set_insert(sfs, pair(t, sig_ta)));
+    } else {
+        return pair(false, null);
+    }
 }
 
 function rule_8(cons, sfs) {
-	const t = head(cons);
-	const ta = tail(cons);
-	if (is_function_type(t) && is_function_type(ta)) {
-		const t_params = param_types_of_fn_type(t);
-		const ta_params = param_types_of_fn_type(ta);
-		if (length(t_params) !== length(ta_params)) {
-			return pair(false, null);
-		} else {
-			// creating n constraints
-			const fn_cons = pair(
-				pair(return_type_of_fn_type(t), return_type_of_fn_type(ta)),
-				zip_list(t_params, ta_params)
-			);
-			return pair(true, accumulate(solve, sfs, fn_cons));
-		}
-	} else {
-		return pair(false, null);
-	}
+    const t = head(cons);
+    const ta = tail(cons);
+    if (is_function_type(t) && is_function_type(ta)) {
+        const t_params = param_types_of_fn_type(t);
+        const ta_params = param_types_of_fn_type(ta);
+        if (length(t_params) !== length(ta_params)) {
+            return pair(false, null);
+        } else {
+            // creating n constraints
+            const fn_cons = pair(
+                pair(return_type_of_fn_type(t), return_type_of_fn_type(ta)),
+                zip_list(t_params, ta_params)
+            );
+            return pair(true, accumulate(solve, sfs, fn_cons));
+        }
+    } else {
+        return pair(false, null);
+    }
 }
 
 function is_type_var(t) {
-	return head(t) === "type_variable";
+    return head(t) === "type_variable";
 }
 
 function is_base_type(t) {
-	return head(t) === "primitive";
+    return head(t) === "primitive";
 }
 
 function is_function_type(t) {
-	return head(t) === "function";
+    return head(t) === "function";
 }
 
 function equal_type(t1, t2) {
-	return is_null(t1) || is_null(t2)
-		? false
-		: head(t1) !== head(t2)
-		? false
-		: is_type_var(t1)
-		? list_ref(t1, 2) === list_ref(t2, 2) // type var are equated by the number
-		: equal(t1, t2);
+    return is_null(t1) || is_null(t2)
+        ? false
+        : head(t1) !== head(t2)
+        ? false
+        : is_type_var(t1)
+        ? list_ref(t1, 2) === list_ref(t2, 2) // type var are equated by the number
+        : equal(t1, t2);
 }
 
 function change_type_var_to_addable(type_var) {
-	if (!is_type_var(type_var)) {
-		error("is not a type var", type_var);
-	} else {
-		return make_new_A_type(head(tail(tail(type_var))));
-	}
+    if (!is_type_var(type_var)) {
+        error("is not a type var", type_var);
+    } else {
+        return make_new_A_type(head(tail(tail(type_var))));
+    }
 }
--- test.js
+++ test.js
@@ -1,175 +1,182 @@
 function print_type(type) {
-	function print_args(ts) {
-		return is_null(ts) ? "null" 
-			: length(ts) === 1
-			? print_type(head(ts))
-			: print_type(head(ts)) + " * " + print_args(tail(ts));
-	}
-	
-	return is_base_type(type)
-		? head(tail(type))
-		: is_function_type(type)
-		? "(" + print_args(param_types_of_fn_type(type)) +
-		  ") > " +
-		  print_type(return_type_of_fn_type(type))
-		: is_type_var(type)
-		? head(tail(type)) + stringify(head(tail(tail(type))))
-		: error("Unknown type: " + stringify(type));
+    function print_args(ts) {
+        return is_null(ts)
+            ? "null"
+            : length(ts) === 1
+            ? print_type(head(ts))
+            : print_type(head(ts)) + " * " + print_args(tail(ts));
+    }
+
+    return is_base_type(type)
+        ? head(tail(type))
+        : is_function_type(type)
+        ? "(" +
+          print_args(param_types_of_fn_type(type)) +
+          ") > " +
+          print_type(return_type_of_fn_type(type))
+        : is_type_var(type)
+        ? head(tail(type)) + stringify(head(tail(tail(type))))
+        : error("Unknown type: " + stringify(type));
 }
 
 function test_1() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "1;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "1 (T1);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "1;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "1 (T1);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
 }
 
 function test_2() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "(()=>1)();";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "((() => (1 (T1)) (T2)) (T3))() (T4);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	// display(solved_form);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_T_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "(()=>1)();";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "((() => (1 (T1)) (T2)) (T3))() (T4);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    // display(solved_form);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_T_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
 }
 
 function test_3() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "true ? 1 : 2;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "(true (T1) ? 1 (T2): 2 (T3)) (T4);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_T_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_A_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "true ? 1 : 2;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "(true (T1) ? 1 (T2): 2 (T3)) (T4);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_T_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_A_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
 }
 
 function test_4() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "1 + 1;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "(1 (T1) + 1 (T2)) (T3);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "1 + 1;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "(1 (T1) + 1 (T2)) (T3);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
 }
 
 function test_5() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "1 + 3 * 4;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "(1 (T1) + (3 (T2) * 4 (T3)) (T4)) (T5);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_A_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
-	display("T5 : " + print_type(sigma(make_new_T_type(5), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "1 + 3 * 4;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "(1 (T1) + (3 (T2) * 4 (T3)) (T4)) (T5);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_A_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("T5 : " + print_type(sigma(make_new_T_type(5), solved_form)));
 }
 
 function test_6() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "(1 + 3) * 4;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "(1 (T1) + 3 (T2)) (T3) * 4 (T4)) (T5);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_A_type(4), solved_form)));
-	display("T5 : " + print_type(sigma(make_new_T_type(5), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "(1 + 3) * 4;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "(1 (T1) + 3 (T2)) (T3) * 4 (T4)) (T5);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_A_type(4), solved_form)));
+    display("T5 : " + print_type(sigma(make_new_T_type(5), solved_form)));
 }
 
 function test_7() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "! (1 === 1);";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "! ((1 (T1) === 1 (T2)) (T3)) (T4);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "! (1 === 1);";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display("Annotated program: " + "! ((1 (T1) === 1 (T2)) (T3)) (T4);");
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
 }
 
 function test_8() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "(! (1 === 1)) ? 1 : 2;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	// display(annotated);
-	display("Annotated program: " + "((! ((1 (T1) === 1 (T2)) (T3)) (T4)) ? 1 (T5) : 2 (T6)) (T7);");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
-	display("T5 : " + print_type(sigma(make_new_A_type(5), solved_form)));
-	display("T6 : " + print_type(sigma(make_new_A_type(6), solved_form)));
-	display("T7 : " + print_type(sigma(make_new_T_type(7), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "(! (1 === 1)) ? 1 : 2;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    // display(annotated);
+    display(
+        "Annotated program: " +
+            "((! ((1 (T1) === 1 (T2)) (T3)) (T4)) ? 1 (T5) : 2 (T6)) (T7);"
+    );
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_A_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("T5 : " + print_type(sigma(make_new_A_type(5), solved_form)));
+    display("T6 : " + print_type(sigma(make_new_A_type(6), solved_form)));
+    display("T7 : " + print_type(sigma(make_new_T_type(7), solved_form)));
 }
 
 function test_9() {
-	display("================================================================");
-	const sigma_set = null;
-	const program = "const x = 1; x;";
-	display("Program: " + program);
-	const annotated = annotate_top_level(parse(program));
-	display(annotated);
-	display("Annotated program: " + "((const x (T1) = 1 (T2)) (T3); x (T1);) (T4)");
-	const transformed = transform_top_level(annotated);
-	const solved_form = collect(transformed, sigma_set);
-	display("Types:");
-	display("T1 : " + print_type(sigma(make_new_T_type(1), solved_form)));
-	display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
-	display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
-	display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
+    display("================================================================");
+    const sigma_set = null;
+    const program = "const x = 1; x;";
+    display("Program: " + program);
+    const annotated = annotate_top_level(parse(program));
+    display(annotated);
+    display(
+        "Annotated program: " + "((const x (T1) = 1 (T2)) (T3); x (T1);) (T4)"
+    );
+    const transformed = transform_top_level(annotated);
+    const solved_form = collect(transformed, sigma_set);
+    display("Types:");
+    display("T1 : " + print_type(sigma(make_new_T_type(1), solved_form)));
+    display("T2 : " + print_type(sigma(make_new_A_type(2), solved_form)));
+    display("T3 : " + print_type(sigma(make_new_T_type(3), solved_form)));
+    display("T4 : " + print_type(sigma(make_new_T_type(4), solved_form)));
 }
 
 // test_1();
