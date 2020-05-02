4fae5f824e395352c1b3b1110d5c5c2e0975f437
Tianyi <42670338+tysng@users.noreply.github.com>
2020-05-02 13:37:24 +0800

    Migrate pack script to node.js

--- src/environment.js
+++ src/environment.js
@@ -97,99 +97,104 @@ function extend_environment(names, types, base_env) {
 
 const the_empty_environment = null;
 
-const mono_prim_ops = list(
-    list("-", list(number_type, number_type), number_type),
-    list("*", list(number_type, number_type), number_type),
-    list("/", list(number_type, number_type), number_type),
-    list("%", list(number_type, number_type), number_type),
-    list("!", list(bool_type), bool_type),
-    list("!", list(bool_type), bool_type),
-    list("-1", list(number_type), number_type) // handled explicitly in annotate_application
-);
+const mono_prim_ops = () =>
+    list(
+        list("-", list(number_type, number_type), number_type),
+        list("*", list(number_type, number_type), number_type),
+        list("/", list(number_type, number_type), number_type),
+        list("%", list(number_type, number_type), number_type),
+        list("!", list(bool_type), bool_type),
+        list("!", list(bool_type), bool_type),
+        list("-1", list(number_type), number_type) // handled explicitly in annotate_application
+    );
 
-const poly_prim_ops = list(
-    list("&&", list(bool_type, T_type), T_type),
-    list("||", list(bool_type, T_type), T_type),
-    list("+", list(A_type, A_type), A_type),
-    list("===", list(A_type, A_type), bool_type),
-    list("!==", list(A_type, A_type), bool_type),
-    list(">", list(A_type, A_type), bool_type),
-    list("<", list(A_type, A_type), bool_type),
-    list(">=", list(A_type, A_type), bool_type),
-    list("<=", list(A_type, A_type), bool_type)
-);
+const poly_prim_ops = () =>
+    list(
+        list("&&", list(bool_type, T_type), T_type),
+        list("||", list(bool_type, T_type), T_type),
+        list("+", list(A_type, A_type), A_type),
+        list("===", list(A_type, A_type), bool_type),
+        list("!==", list(A_type, A_type), bool_type),
+        list(">", list(A_type, A_type), bool_type),
+        list("<", list(A_type, A_type), bool_type),
+        list(">=", list(A_type, A_type), bool_type),
+        list("<=", list(A_type, A_type), bool_type)
+    );
 
-const build_in_poly_func = list(
-    list("display", list(T_type), undefined_type),
-    list("error", list(T_type), undefined_type),
-    list("is_boolean", list(T_type), bool_type),
-    list("is_boolean", list(T_type), bool_type),
-    list("is_function", list(T_type), bool_type),
-    list("is_number", list(T_type), bool_type),
-    list("is_string", list(T_type), bool_type),
-    list("is_undefined", list(T_type), bool_type),
-    list("stringify", list(T_type), string_type),
-    list("math_hypot", list(T_type), undefined_type)
-);
+const build_in_poly_func = () =>
+    list(
+        list("display", list(T_type), undefined_type),
+        list("error", list(T_type), undefined_type),
+        list("is_boolean", list(T_type), bool_type),
+        list("is_boolean", list(T_type), bool_type),
+        list("is_function", list(T_type), bool_type),
+        list("is_number", list(T_type), bool_type),
+        list("is_string", list(T_type), bool_type),
+        list("is_undefined", list(T_type), bool_type),
+        list("stringify", list(T_type), string_type),
+        list("math_hypot", list(T_type), undefined_type)
+    );
 
-const built_in_mono_func = list(
-    list("math_abs", list(number_type), number_type),
-    list("math_acos", list(number_type), number_type),
-    list("math_acosh", list(number_type), number_type),
-    list("math_asin", list(number_type), number_type),
-    list("math_asinh", list(number_type), number_type),
-    list("math_atan", list(number_type), number_type),
-    list("math_atan2", list(number_type, number_type), number_type),
-    list("math_atanh", list(number_type), number_type),
-    list("math_cbrt", list(number_type), number_type),
-    list("math_ceil", list(number_type), number_type),
-    list("math_clz32", list(number_type), number_type),
-    list("math_cos", list(number_type), number_type),
-    list("math_cosh", list(number_type), number_type),
-    list("math_exp", list(number_type), number_type),
-    list("math_expm1", list(number_type), number_type),
-    list("math_floor", list(number_type), number_type),
-    list("math_fround", list(number_type), number_type),
-    list("math_imul", list(number_type, number_type), number_type),
-    list("math_log", list(number_type), number_type),
-    list("math_log1p", list(number_type), number_type),
-    list("math_log2", list(number_type), number_type),
-    list("math_log10", list(number_type), number_type),
-    list("math_pow", list(number_type, number_type), number_type),
-    list("math_random", list(null_type), number_type),
-    list("math_round", list(number_type), number_type),
-    list("math_sign", list(number_type), number_type),
-    list("math_sin", list(number_type), number_type),
-    list("math_sinh", list(number_type), number_type),
-    list("math_sqrt", list(number_type), number_type),
-    list("math_tan", list(number_type), number_type),
-    list("math_tanh", list(number_type), number_type),
-    list("math_trunc", list(number_type), number_type),
-    list("parse_int", list(string_type, number_type), number_type),
-    list("prompt", list(string_type), string_type),
-    list("runtime", list(null_type), number_type)
-);
+const built_in_mono_func = () =>
+    list(
+        list("math_abs", list(number_type), number_type),
+        list("math_acos", list(number_type), number_type),
+        list("math_acosh", list(number_type), number_type),
+        list("math_asin", list(number_type), number_type),
+        list("math_asinh", list(number_type), number_type),
+        list("math_atan", list(number_type), number_type),
+        list("math_atan2", list(number_type, number_type), number_type),
+        list("math_atanh", list(number_type), number_type),
+        list("math_cbrt", list(number_type), number_type),
+        list("math_ceil", list(number_type), number_type),
+        list("math_clz32", list(number_type), number_type),
+        list("math_cos", list(number_type), number_type),
+        list("math_cosh", list(number_type), number_type),
+        list("math_exp", list(number_type), number_type),
+        list("math_expm1", list(number_type), number_type),
+        list("math_floor", list(number_type), number_type),
+        list("math_fround", list(number_type), number_type),
+        list("math_imul", list(number_type, number_type), number_type),
+        list("math_log", list(number_type), number_type),
+        list("math_log1p", list(number_type), number_type),
+        list("math_log2", list(number_type), number_type),
+        list("math_log10", list(number_type), number_type),
+        list("math_pow", list(number_type, number_type), number_type),
+        list("math_random", list(null_type), number_type),
+        list("math_round", list(number_type), number_type),
+        list("math_sign", list(number_type), number_type),
+        list("math_sin", list(number_type), number_type),
+        list("math_sinh", list(number_type), number_type),
+        list("math_sqrt", list(number_type), number_type),
+        list("math_tan", list(number_type), number_type),
+        list("math_tanh", list(number_type), number_type),
+        list("math_trunc", list(number_type), number_type),
+        list("parse_int", list(string_type, number_type), number_type),
+        list("prompt", list(string_type), string_type),
+        list("runtime", list(null_type), number_type)
+    );
 
-const built_in_const = list(
-    pair("Infinity", number_type),
-    pair("math_LN2", number_type),
-    pair("math_LN10", number_type),
-    pair("math_LOG2E", number_type),
-    pair("math_LOG10E", number_type),
-    pair("math_SQRT1_2", number_type),
-    pair("NaN", number_type),
-    pair("undefined", undefined_type)
-);
+const built_in_const = () =>
+    list(
+        pair("Infinity", number_type),
+        pair("math_LN2", number_type),
+        pair("math_LN10", number_type),
+        pair("math_LOG2E", number_type),
+        pair("math_LOG10E", number_type),
+        pair("math_SQRT1_2", number_type),
+        pair("NaN", number_type),
+        pair("undefined", undefined_type)
+    );
 
 function setup_environment() {
-    const mono_func = append(mono_prim_ops, built_in_mono_func);
+    const mono_func = append(mono_prim_ops(), built_in_mono_func());
     const mono_func_names = map((l) => head(l), mono_func);
     const mono_func_values = map(
         (l) => make_function_type(head(tail(l)), head(tail(tail(l)))),
         mono_func
     );
 
-    const poly_func = append(poly_prim_ops, build_in_poly_func);
+    const poly_func = append(poly_prim_ops(), build_in_poly_func());
     const poly_func_names = map((l) => head(l), poly_func);
     const poly_func_values = map(
         (l) =>
@@ -199,8 +204,8 @@ function setup_environment() {
         poly_func
     );
 
-    const built_in_const_names = map((l) => head(l), built_in_const);
-    const built_in_const_values = map((l) => tail(l), built_in_const);
+    const built_in_const_names = map((l) => head(l), built_in_const());
+    const built_in_const_values = map((l) => tail(l), built_in_const());
 
     return extend_environment(
         append(mono_func_names, append(poly_func_names, built_in_const_names)),
